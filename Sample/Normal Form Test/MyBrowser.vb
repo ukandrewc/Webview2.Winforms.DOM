d52ac058dc70ed8a540eddd6faa95b50a421e8ac
ukandrewc <ukandrewc@gmail.com>
2021-04-11 15:49:40 +0100

    Delete Sample directory

--- Sample/Normal Form Test/MyBrowser.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class MyBrowser : Inherits WVBrowser
-
-	Private Sub MyBrowser_DOMContentLoaded(sender As Object) Handles Me.DOMContentLoaded
-		ExecuteScriptAsync("alert('My Browser')")
-	End Sub
-End Class
